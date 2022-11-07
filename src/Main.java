7b4dbaffc59cdb9dd16e356826d32dbb8dba245c
Avasov Syimyk <sysya1304@gmail.com>
2022-11-07 17:00:39 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,56 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        FileWriter writer = new FileWriter("java.txt");
+        writer.write("A a\n" +
+                "B b\n" +
+                 "C c\n" +
+                  "D d\n" +
+                   "E e\n" +
+                    "F f\n" +
+                     "G g\n" +
+                      "H h\n" +
+                       "I i\n" +
+                        "J j\n" +
+                         "K k\n" +
+                          "L l\n" +
+                           "M m\n" +
+                            "N n\n" +
+                             "O o\n" +
+                              "P p\n" +
+                               "Q q\n" +
+                              "R r\n" +
+                             "S s\n" +
+                            "T t\n" +
+                           "U u\n" +
+                          "V v\n" +
+                         "W w\n" +
+                        "X x\n" +
+                       "Y y\n" +
+                      "Z z\n" +
+                     "1\n" +
+                    "2\n" +
+                   "3\n" +
+                  "4\n" +
+                 "5\n" +
+                "6\n" +
+                 "7\n" +
+                  "8\n" +
+                   "9\n" +
+                    "0\n");
+        writer.close();
+
+        FileReader song = new FileReader("java.txt");
+        Scanner scanner = new Scanner(song);
+        StringBuilder stringBuilder = new StringBuilder();
+           int a= 0;
+        while (scanner.hasNextLine()){
+            System.out.println(++a+": "+scanner.nextLine());
+            }
+    }
+}
+
